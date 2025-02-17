78c1ad2cc0da78ab0848535a362bdf5fd38c5ce0
amin-nassar <amin.nassar.ce@gmail.com>
2025-02-17 20:45:48 +0200

    Build Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
 export default function Page() {
-  return <h1 className="text-2xl">Home</h1>;
+  return (
+    <section className="hero-container">
+      <h1 className="heading">
+        Pitch your Startup, <br /> Connect with Entrepreneurs
+      </h1>
+
+      <p className="sub-heading !max-w-3xl">
+        Share your ideas, gather votes, and gain the traction needed to bring
+        your startup to life.
+      </p>
+    </section>
+  );
 }
