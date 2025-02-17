f54af17ac36069b3a59e1892ade8b3c55f4c405d
amin-nassar <amin.nassar.ce@gmail.com>
2025-02-17 23:09:30 +0200

    Build Startups' Grid and Single Startup's Card components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,54 @@
-import { SearchForm } from "@/app/components";
-
+import { SearchForm, StartupCard } from "@/app/components";
+import { Post } from "../components/StartupCard/StartupCard";
 interface IPage {
   searchParams: Awaited<{ query?: string }>;
 }
 
 export default async function Page({ searchParams }: IPage) {
   const { query } = await searchParams;
+
+  const posts: Post[] = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      _id: 1,
+      author: { _id: 1, name: "Amin" },
+      category: "Health",
+      description: "Description",
+      image:
+        "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "iO Health",
+    },
+  ];
+
   return (
-    <section className="hero-container">
-      <h1 className="heading">
-        Pitch your Startup, <br /> Connect with Entrepreneurs
-      </h1>
+    <>
+      <section className="hero-container">
+        <h1 className="heading">
+          Pitch your Startup, <br /> Connect with Entrepreneurs
+        </h1>
 
-      <p className="sub-heading !max-w-3xl">
-        Share your ideas, gather votes, and gain the traction needed to bring
-        your startup to life.
-      </p>
+        <p className="sub-heading !max-w-3xl">
+          Share your ideas, gather votes, and gain the traction needed to bring
+          your startup to life.
+        </p>
 
-      <SearchForm query={query} />
-    </section>
+        <SearchForm query={query} />
+      </section>
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for: "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card-grid">
+          {posts.length ? (
+            posts.map((post) => {
+              return <StartupCard key={post._id} post={post} />;
+            })
+          ) : (
+            <p className="text-black-100 text-sm font-normal"></p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
---
+++ app/components/StartupCard/StartupCard.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Eye } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export interface Post {
+  _id: number;
+  _createdAt: Date;
+  views: number;
+  author: {
+    _id: number;
+    name: string;
+  };
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+}
+
+interface Props {
+  post: Post;
+}
+
+function StartupCard({ post }: Props) {
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup-card-date">
+          {post._createdAt.toLocaleString("en-US", {
+            month: "long",
+            day: "2-digit",
+            year: "numeric",
+          })}
+        </p>
+        <div className="flex gap-1.5">
+          <Eye className="size-6 text-primary" />
+          <span className="text-16-medium">{post.views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${post.author._id}`}>
+            <p className="text-16-medium line-clamp-1">{post.author.name}</p>
+          </Link>
+
+          <Link href={`/startup/${post._id}`}>
+            <h3 className="text-26-semibold">{post.title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${post.author._id}`}>
+          <Image
+            src="https://i.pravatar.cc/500"
+            alt="placeholder"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${post._id}`}>
+        <p className="startup-card-desc">{post.description}</p>
+
+        <img src={post.image} alt="placeholder" className="startup-card-img" />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/query=${post.category.toLowerCase()}`}>
+          <p className="text-16-medium">{post.category}</p>
+        </Link>
+        <Button className="startup-card-btn">
+          <Link href={`/startup/${post._id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+}
+
+export default StartupCard;
