470d90d2598338287de524bea563060452e29fc6
amin-nassar <amin.nassar.ce@gmail.com>
2025-02-24 00:15:50 +0200

    Add Authenticstion & Submission to Startup Form.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { PropsWithChildren } from "react";
 import { Navbar } from "@/app/components";
+import { Toaster } from "@/components/ui/sonner";
 
 function Layout({ children }: PropsWithChildren) {
   return (
     <main className="font-work-sans">
       <Navbar />
       {children}
+      <Toaster richColors theme="light" />
     </main>
   );
 }
--- app/components/StartupForm/StartupForm.tsx
+++ app/components/StartupForm/StartupForm.tsx
@@ -2,16 +2,77 @@
 
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import React, { useState } from "react";
+import React, { useState, useActionState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
+import { formSchema } from "@/lib/validation";
+import { z } from "zod";
+import { toast } from "sonner";
+import { createPitch } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 function StartupForm() {
+  const router = useRouter();
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const handleSubmitForm = async (prevState: any, formData: FormData) => {
+    try {
+      const formValues = {
+        title: formData.get("title"),
+        description: formData.get("description"),
+        category: formData.get("category"),
+        link: formData.get("link"),
+        pitch,
+      };
+
+      await formSchema.parseAsync(formValues);
+
+      const result = await createPitch(prevState, formData, pitch);
+
+      if (result.status === "SUCCESS") {
+        toast.success("Success", {
+          description: "Your startup pitch has been created successfully",
+        });
+
+        router.push(`/startup/${result._id}`);
+      }
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErrors = error.flatten().fieldErrors;
+        setErrors(fieldErrors as unknown as Record<string, string>);
+
+        toast.error("Error", {
+          description: "Please check your inputs & try again.",
+        });
+
+        return { ...prevState, error: "Validation failed", status: "ERROR" };
+      }
+
+      toast.error("Error", {
+        description: "An unexpected error has occurred",
+      });
+
+      return {
+        ...prevState,
+        error: "An unexpected error has occurred",
+        status: "ERROR",
+      };
+    }
+  };
+
+  const INITIAL_STATE = {
+    status: "INITIAL",
+  };
+
+  const [_, formAction, isPending] = useActionState(
+    handleSubmitForm,
+    INITIAL_STATE
+  );
+
   const [pitch, setPitch] = useState("");
 
   return (
-    <form action={() => {}} className="startup-form">
+    <form action={formAction} className="startup-form">
       <div>
         <label className="startup-form-label" htmlFor="title">
           Title
@@ -23,6 +84,7 @@ function StartupForm() {
           required
           placeholder="Startup Title"
         />
+        {errors.title && <p className="startup-form-error">{errors.title}</p>}
       </div>
 
       <div>
@@ -36,6 +98,9 @@ function StartupForm() {
           required
           placeholder="Startup Description"
         />
+        {errors.description && (
+          <p className="startup-form-error">{errors.description}</p>
+        )}
       </div>
 
       <div>
@@ -49,6 +114,9 @@ function StartupForm() {
           required
           placeholder="Startup Category: Tech, Heatlh, Education"
         />
+        {errors.category && (
+          <p className="startup-form-error">{errors.category}</p>
+        )}
       </div>
 
       <div>
@@ -62,6 +130,7 @@ function StartupForm() {
           required
           placeholder="Startup Image URL"
         />
+        {errors.link && <p className="startup-form-error">{errors.link}</p>}
       </div>
 
       <div data-color-mode="light">
@@ -80,10 +149,15 @@ function StartupForm() {
             placeholder: "Briefly describe your idea what problem it solves",
           }}
         />
+        {errors.pitch && <p className="startup-form-error">{errors.pitch}</p>}
       </div>
 
-      <Button type="submit" className="startup-form-btn text-white">
-        Submit your pitch
+      <Button
+        type="submit"
+        className="startup-form-btn text-white"
+        disabled={isPending}
+      >
+        {isPending ? "Submitting..." : "Submit your pitch"}
         <Send className="!size-4" />
       </Button>
     </form>
